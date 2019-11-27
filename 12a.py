4194a13cd64397f3b24c39d6eb6bf10b0fc3eef0
patrickhyena <56729438+patrickhyena@users.noreply.github.com>
2019-11-27 11:52:43 +0700

    number twelve

--- 12a.py
+++ 12a.py
@@ -4,7 +4,7 @@ def caeser_cipher(str):
     alphabet = string.ascii_lowercase
     new_word = ""
     for x in str:
-        next = alphabet.find(x) + 13
+        next = alphabet.find(x) + 3
         if next > 26:
             next = next-26
         new_letter = alphabet[next]
